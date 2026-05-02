b9163441b24941014688bec928d523e837c26e3c
halima <dahunsiolajumoke18@gmail.com>
2026-05-02 17:18:21 +0100

    Added dark mode feature

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import { redirect } from "next/navigation";
 
-export const page = () => {
-    return (
-        <div>page</div>
-    )
+export default function DashboardPage() {
+    redirect("/dashboard/overview");
 }
